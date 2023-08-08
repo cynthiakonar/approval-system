2e3e754665ce7c033661fd3f45407e30aca50d79
Cynthia <cynthia.konar@gmail.com>
2023-08-08 19:40:28 +0530

    feature: request screen ui

--- lib/screens/requester/widgets/new_request_dialog.dart
+++ lib/screens/requester/widgets/new_request_dialog.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+import 'package:file_picker/file_picker.dart';
+import '../../../utils/constants.dart';
 
 class NewRequestDialog extends StatefulWidget {
   NewRequestDialog({Key? key}) : super(key: key);
@@ -11,21 +15,28 @@ class NewRequestDialog extends StatefulWidget {
 class _NewRequestDialogState extends State<NewRequestDialog> {
   bool _isLoading = false;
 
+  final ItemEditingController = TextEditingController();
   final reasonController = TextEditingController();
-
+  String? siteValue;
   final _reasonFormKey = GlobalKey<FormState>();
+  List<String> siteItems = ["x", "y"];
+  String? imageFilePath;
+  List<Map<String, String?>> material_quantity = [];
+
+  bool _isFileAdded = false;
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
+      backgroundColor: secondaryColor,
       title: Stack(
         children: [
-          Center(
+          const Center(
             child: Text(
-              "Apply for leave",
+              "Request for workflow",
               style: TextStyle(
                 fontSize: 14,
-                fontWeight: FontWeight.w700,
+                fontWeight: FontWeight.w600,
               ),
             ),
           ),
@@ -45,7 +56,7 @@ class _NewRequestDialogState extends State<NewRequestDialog> {
       ),
       content: SingleChildScrollView(
         child: _isLoading
-            ? Center(
+            ? const Center(
                 child: Column(
                   children: [
                     SpinKitFadingCircle(
@@ -72,19 +83,11 @@ class _NewRequestDialogState extends State<NewRequestDialog> {
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text(
-                          "Select dates :",
+                        const Text(
+                          "Request Name :",
                           style: TextStyle(
-                            fontSize: 13,
-                            fontWeight: FontWeight.w700,
-                          ),
-                        ),
-                        const SizedBox(height: 10),
-                        Text(
-                          "Reason :",
-                          style: TextStyle(
-                            fontSize: 13,
-                            fontWeight: FontWeight.w700,
+                            fontSize: 12,
+                            fontWeight: FontWeight.w500,
                           ),
                         ),
                         const SizedBox(height: 8),
@@ -116,7 +119,7 @@ class _NewRequestDialogState extends State<NewRequestDialog> {
                             ),
                             focusedBorder: OutlineInputBorder(
                               borderSide: BorderSide(
-                                color: Color(0xFF7B931B),
+                                color: Colors.white70,
                               ),
                             ),
                             enabledBorder: OutlineInputBorder(
@@ -124,13 +127,233 @@ class _NewRequestDialogState extends State<NewRequestDialog> {
                                 5,
                               ),
                               borderSide: BorderSide(
-                                color: Color(0xFF7B931B),
+                                color: Colors.white70,
                               ),
                             ),
                             isDense: true,
                             contentPadding: EdgeInsets.fromLTRB(10, 20, 10, 0),
                           ),
                         ),
+                        const SizedBox(height: 8),
+                        const Text(
+                          "Workflow Type :",
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        DropdownButtonFormField<String>(
+                          itemHeight: null,
+                          decoration: InputDecoration(
+                            // isDense: true,
+                            isCollapsed: true,
+                            contentPadding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Colors.white70,
+                              ),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Colors.white70,
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Colors.white70,
+                              ),
+                            ),
+                          ),
+                          autofocus: false,
+                          value: siteValue,
+                          isExpanded: true,
+                          iconSize: 24,
+                          icon: Icon(
+                            Icons.keyboard_arrow_down,
+                            color: Colors.white70,
+                          ),
+                          items: siteItems.map(buildMenuItem).toList(),
+                          onChanged: (Evalue) => setState(
+                            () {
+                              this.siteValue = Evalue;
+                              ItemEditingController.text = Evalue!;
+                            },
+                          ),
+                          onSaved: (Evalue) {
+                            ItemEditingController.text = Evalue!;
+                          },
+                          validator: (value) {
+                            if (ItemEditingController.text.isEmpty) {
+                              return "*Required";
+                            }
+                            return null;
+                          },
+                        ),
+                        const SizedBox(height: 8),
+                        const Text(
+                          "Description :",
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        TextFormField(
+                          onChanged: (value) {
+                            setState(() {
+                              reasonController.text = value;
+                            });
+                          },
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return "*Required";
+                            }
+                            return null;
+                          },
+                          keyboardType: TextInputType.text,
+                          minLines: 1,
+                          maxLines: 5,
+                          textCapitalization: TextCapitalization.sentences,
+                          style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 14,
+                          ),
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(
+                                5,
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Colors.white70,
+                              ),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(
+                                5,
+                              ),
+                              borderSide: BorderSide(
+                                color: Colors.white70,
+                              ),
+                            ),
+                            isDense: true,
+                            contentPadding: EdgeInsets.fromLTRB(10, 20, 10, 0),
+                          ),
+                        ),
+                        SizedBox(height: 8),
+                        const Text(
+                          "Add Attachment (if any) :",
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        _isFileAdded
+                            ? GestureDetector(
+                                onTap: () => setState(() {
+                                  _isFileAdded = false;
+                                  imageFilePath = null;
+                                }),
+                                child: Container(
+                                  padding: EdgeInsets.all(8.0),
+                                  decoration: BoxDecoration(
+                                    color: Colors.white70,
+                                    borderRadius: BorderRadius.circular(6.0),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.black12,
+                                        blurRadius: 2.0,
+                                        spreadRadius: 1.0,
+                                        offset: Offset(0, 2),
+                                      ),
+                                    ],
+                                  ),
+                                  child: Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      Text(
+                                        'File added',
+                                        // style: TT.f13w600
+                                        //     .copyWith(color: ColorTheme.iconColor),
+                                      ),
+                                      Icon(
+                                        Icons.close,
+                                        size: 20,
+                                        // color: ColorTheme.iconColor,
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              )
+                            : GestureDetector(
+                                onTap: material_quantity.isNotEmpty
+                                    ? null
+                                    : () async {
+                                        try {
+                                          FilePickerResult? result =
+                                              await FilePicker.platform
+                                                  .pickFiles(
+                                                      type: FileType.custom,
+                                                      allowedExtensions: [
+                                                'pdf',
+                                                'jpg',
+                                                'png',
+                                                'webp'
+                                              ]);
+                                          if (result != null) {
+                                            setState(() {
+                                              _isFileAdded = true;
+                                              imageFilePath =
+                                                  result.files.first.path;
+                                            });
+
+                                            print('File path: ' +
+                                                result.files.single.path!);
+                                          } else {
+                                            print('User cancelled file pick');
+                                          }
+                                        } on PlatformException {
+                                          ScaffoldMessenger.of(context)
+                                              .showSnackBar(
+                                            SnackBar(
+                                              content: Text(
+                                                  'File access permission denied'),
+                                              duration: Duration(seconds: 2),
+                                            ),
+                                          );
+                                        }
+                                      },
+                                child: Container(
+                                  width: double.infinity,
+                                  height: 40,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                      color: Colors.white70,
+                                      width: 1,
+                                    ),
+                                    borderRadius: BorderRadius.circular(7),
+                                    color: material_quantity.isEmpty
+                                        ? Colors.transparent
+                                        : Colors.white70,
+                                  ),
+                                  child: Center(
+                                    child: Text(
+                                      "Choose file",
+                                      style: TextStyle(
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.w600,
+                                        // color: (material_quantity.isEmpty)
+                                        //     ? ColorTheme.iconColor
+                                        //     : Colors.black26
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                        SizedBox(height: 20),
                       ],
                     ),
                   ),
@@ -144,14 +367,14 @@ class _NewRequestDialogState extends State<NewRequestDialog> {
                           onPressed:
                               (reasonController.text.isEmpty) ? null : () {},
                           style: ElevatedButton.styleFrom(
-                            backgroundColor: const Color(0xFF7B931B),
+                            backgroundColor: Colors.blueAccent,
                             elevation: 0,
                             shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(6),
                             ),
                           ),
                           child: const Text(
-                            'Apply for leave',
+                            'Submit',
                             style: TextStyle(
                               color: Colors.white,
                               fontSize: 14,
@@ -167,4 +390,14 @@ class _NewRequestDialogState extends State<NewRequestDialog> {
       ),
     );
   }
+
+  DropdownMenuItem<String> buildMenuItem(String item) => DropdownMenuItem(
+      value: item,
+      child: Text(
+        item,
+        style: TextStyle(
+          fontWeight: FontWeight.w400,
+          fontSize: 14,
+        ),
+      ));
 }
