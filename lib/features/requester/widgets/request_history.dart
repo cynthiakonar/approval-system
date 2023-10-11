124a0fff775f4f9b48167db9bbf129b164ec2ebf
Priyam <priyam05soni@gmail.com>
2023-10-11 17:35:42 +0530

    fix file upload

--- lib/features/requester/widgets/request_history.dart
+++ lib/features/requester/widgets/request_history.dart
@@ -1,9 +1,16 @@
+// ignore_for_file: avoid_print, no_leading_underscores_for_local_identifiers
+
+import 'dart:html';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:intl/intl.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:path_provider/path_provider.dart';
 import '../../../../utils/constants.dart';
 import 'edit_request_dialog.dart';
+import 'package:http/http.dart' as http;
 
 class RequestHistory extends StatefulWidget {
   RequestHistory({
@@ -22,6 +29,44 @@ class _RequestHistoryState extends State<RequestHistory> {
       context: this.context,
       builder: (context) => EditRequestDialog(request: request),
     );
+  }
+
+  // Future<void> downloadFile(request) async {
+  //   var dir = (await DownloadsPath.downloadsDirectory())?.path;
+  //   try {
+  //     // Extract the file extension from the URL
+  //     String fileExtension = request["attachmentUrl"].split('.').last;
+
+  //     if (fileExtension == "pdf") {
+  //       print("The file is a PDF.");
+  //     }
+
+  //     await Dio().download(request["attachmentUrl"], dir,
+  //         onReceiveProgress: (received, total) {
+  //       if (total != -1) {}
+  //     });
+  //     print("File is saved to download folder.");
+  //     // ignore: deprecated_member_use
+  //   } on DioError catch (e) {
+  //     print(e.message);
+  //   }
+  // }
+
+  void downloadTHEFile(request) {
+    // Create a hidden anchor tag with the download URL.
+
+    Uri uri = Uri.parse(request["attachmentUrl"]);
+
+    // Get the path from the URL
+    String path = uri.path;
+
+    // Extract the filename from the path
+    String filename = path.split('/').last;
+
+    final anchor = AnchorElement(href: request["attachmentUrl"])
+      ..target = 'download'
+      ..download = filename
+      ..click();
   }
 
   @override
@@ -59,6 +104,9 @@ class _RequestHistoryState extends State<RequestHistory> {
                 DataColumn(
                   label: Text(""),
                 ),
+                DataColumn(
+                  label: Text(""),
+                ),
               ],
               rows: List.generate(
                 widget.requests.length,
@@ -83,26 +131,23 @@ class _RequestHistoryState extends State<RequestHistory> {
                       height: 30,
                       width: 30,
                     )
-                  : CachedNetworkImage(
-                      fit: BoxFit.cover,
-                      imageUrl: request["attachmentUrl"],
-                      imageBuilder: (context, imageProvider) => Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          image: DecorationImage(
-                              image: imageProvider, fit: BoxFit.cover),
-                          gradient: const LinearGradient(colors: [
-                            Colors.transparent,
-                            Colors.black,
-                            Colors.black,
-                            Colors.black,
-                          ]),
+                  : SizedBox(
+                      height: 30,
+                      width: 30,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(6),
+                        child: CachedNetworkImage(
+                          fit: BoxFit.fitHeight,
+                          imageUrl: request["attachmentUrl"],
+                          height: 30,
+                          width: 30,
+                          placeholder: (context, url) =>
+                              const Center(child: CircularProgressIndicator()),
+                          errorWidget: (context, url, error) => const Center(
+                            child: Icon(Icons.broken_image_outlined),
+                          ),
                         ),
                       ),
-                      placeholder: (context, url) =>
-                          const Center(child: CircularProgressIndicator()),
-                      errorWidget: (context, url, error) => Center(
-                          child: const Icon(Icons.broken_image_outlined)),
                     ),
               Expanded(
                 child: Padding(
@@ -119,6 +164,15 @@ class _RequestHistoryState extends State<RequestHistory> {
             .format(request["dateTime"].toDate())
             .toString())),
         DataCell(Text(request["status"])),
+        request["attachmentUrl"].isNotEmpty
+            ? DataCell(
+                InkWell(
+                    onTap: () async {
+                      downloadTHEFile(request);
+                    },
+                    child: const Icon(Icons.downloading_rounded)),
+              )
+            : const DataCell(Text("")),
         DataCell(
           request["status"] == "Justification Required"
               ? InkWell(
